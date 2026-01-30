5e7922f95e1746ce183e4263ac048523413d7ee3
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2026-01-30 18:33:59 +0900

    로고 추가

--- src/splash_screen/lib/main.dart
+++ src/splash_screen/lib/main.dart
@@ -4,8 +4,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Image.asset('asset/img/logo.png'),
+        /// 335CB0
+        backgroundColor: Color(0xFF335CB0),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo.png',
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        )
       ),
     ),
   );
