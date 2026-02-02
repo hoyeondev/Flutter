cfda2c1953ebb43ee88cfe05da01f9d7e0a38d4e
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2026-02-02 18:03:28 +0900

    stless 사용

--- src/splash_screen/lib/main.dart
+++ src/splash_screen/lib/main.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        /// 335CB0
+      home: HomeScreen(),
+    ),
+  );
+}
+
+
+/// stless
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /// 335CB0
         backgroundColor: Color(0xFF335CB0),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -17,7 +29,8 @@ void main() {
             ),
           ],
         )
-      ),
-    ),
-  );
+    );
+  }
 }
+
+
